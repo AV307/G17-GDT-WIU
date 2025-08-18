804573d8bb78cdf28b157f68607f42504b8f89ea
benawidkhowtocode <i8144406@gmail.com>
2025-08-18 17:24:43 +0800

    enemy

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,1 +1,81 @@
 #include "Enemy.h"
+#include<vector>
+#include<string>
+#include <iostream>
+using namespace std;
+
+int main() {
+    vector<Enemy> enemies;
+
+    enemies.push_back(Enemy("Slime", 10, 5, 3, 2, 1, 10));
+    enemies.push_back(Enemy("Kobold", 20, 10, 10, 2, 5, 20));
+    enemies.push_back(Enemy("wolf", 30, 20, 20, 5, 10, 30));
+
+    Enemy& slime = enemies[0];
+    Enemy& Kobold = enemies[1];
+    Enemy& wolf = enemies[2];
+
+    cout << "Encountered: " << slime.getName()
+        << " with " << slime.getHealth() << " HP!\n";
+
+    slime.takeDamage(4);
+
+    cout << slime.getName() << " now has "
+        << slime.getHealth() << " HP.\n";
+
+    return 0;
+
+}
+
+Enemy::Enemy(string n, int h, int a, int dmg, int def, int s, int r)
+{
+
+}
+
+string Enemy::getName() const
+{
+    return string();
+}
+
+int Enemy::getHealth() const
+{
+    return health;
+}
+
+int Enemy::getAttack() const
+{
+    return attack;
+}
+
+int Enemy::getDamage() const
+{
+    return damage;
+}
+
+int Enemy::getDefence() const
+{
+    return defence;
+}
+
+int Enemy::getSpeed() const
+{
+    return speed;
+}
+
+int Enemy::getReward() const
+{
+    return reward;
+}
+
+void Enemy::takeDamage(int dmg)
+{
+}
+
+bool Enemy::isAlive() const
+{
+    return false;
+}
+
+Enemy::~Enemy()
+{
+}
--- Enemy.h
+++ Enemy.h
@@ -1,7 +1,79 @@
 #pragma once
 #include "Entity.h"
-class Enemy :
-    public Entity
-{
+#include<string>
+#include<vector>
+using namespace std;
+
+//class Enemy :  public Entity
+//{
+//private:
+//	string name;
+//	int health;
+//	int attack;
+//	int damage;
+//	int defence;
+//	int speed;
+//	int reward;
+//
+//
+//
+//public:
+//	Enemy(string n, int h, int a, int dmg, int def, int s) :name(n), health(h), attack(a), damage(dmg), defence(def), speed(s) {};
+//
+//	string getName() const {
+//		return name;
+//	}
+//
+//	int getHealth() const {
+//		return health;
+//	}
+//
+//	int getAttack() const {
+//		return health;
+//	}
+//
+//	int getDamage() const {
+//		return health;
+//	}
+//
+//	int getDefence() const {
+//		return health;
+//	}
+//
+//	int getSpeed() const {
+//		return health;
+//	}
+//};
+
+
+
+class Enemy {
+private:
+    string name;
+    int health;
+    int attack;
+    int damage;
+    int defence;
+    int speed;
+    int reward;
+
+public:
+    // Constructor
+    Enemy(string n, int h, int a, int dmg, int def, int s, int r);
+
+    // Getters
+    string getName() const;
+    int getHealth() const;
+    int getAttack() const;
+    int getDamage() const;
+    int getDefence() const;
+    int getSpeed() const;
+    int getReward() const;
+
+    // Combat
+    void takeDamage(int dmg);
+    bool isAlive() const;
+
+    ~Enemy();
 };
 

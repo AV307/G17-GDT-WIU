6ec9bdca474625ec025c987ac08821f5cc2f4635
benawidkhowtocode <i8144406@gmail.com>
2025-08-20 00:49:07 +0800

    frevdvvf

--- Entity.cpp
+++ Entity.cpp
@@ -1,104 +1,89 @@
 #include "Entity.h"
 
-Entity::Entity()
-{
 
-}
 
-void Entity::setHealth(int hp)
-{
-	hp = 0;
-}
-
-void Entity::setAttack(int atk)
-{
-	atk = 0;
-}
-
-void Entity::setDamage(int dmg)
-{
-	dmg = 0;
-}
-void Entity::setDefense(int defence)
-{
-}
-//void Entity::setDefence(int def)
-//{
-//	def = 0;
-//}
-
-void Entity::setXP(int xp)
-{
-	xp = 0;
-}
-
-void Entity::setGold(int gold)
-{
-	gold = 0;
-}
-
-void Entity::setLvl(int lvl)
-{
-	lvl = 1;
-}
-
-void Entity::setCritDMG(int critDMG)
-{
-	critDMG = 0;
-}
-void Entity::setCritRate(int critRate)
-{
-	critRate = 0;
-}
-
-int Entity::getHealth() const
-{
-	return health;
-}
-
-int Entity::getAttack() const
-{
-	return attack;
-}
-
-int Entity::getDamage() const
-{
-	return damage;
-}
-
-int Entity::getDefense() const
-{
-	return defence;
-}
-
-int Entity::getXP() const
-{
-	return xp;
-}
-
-int Entity::getGold() const
-{
-	return gold;
-}
-
-int Entity::getLvl() const
-{
-	return getLvl();
-}
-int Entity::getCRITDMG() const
-{
-	return CRITDMG;
-}
-int Entity::getCRITRate() const
-{
-	return CRITRate;
+Entity::Entity() : health(100), attack(10), damage(5), defence(5), xp(0), gold(0), level(1), CRITDMG(2), CRITRate(10) {
 }
 
 
+Entity::Entity(int hp, int atk, int dmg, int def, int exp, int g, int lvl, int critdmg, int critrate)
+    : health(hp), attack(atk), damage(dmg), defence(def),
+    xp(exp), gold(g), level(lvl), CRITDMG(critdmg), CRITRate(critrate) {
+}
+
+int Entity::getHealth() const { 
+    return health; 
+}
+
+int Entity::getAttack() const {
+    return attack; 
+}
+
+int Entity::getDamage() const { 
+    return damage; 
+}
+
+int Entity::getDefense() const { 
+    return defence; 
+}
+
+int Entity::getXP() const { 
+    return xp; 
+}
+
+int Entity::getGold() const { 
+    return gold; 
+}
+
+int Entity::getLvl() const { 
+    return level; 
+}
+
+int Entity::getCRITDMG() const { 
+    return CRITDMG; 
+}
+int Entity::getCRITRate() const { 
+    return CRITRate; 
+}
+
+void Entity::setHealth(int hp) { 
+    health = hp; 
+}
+
+void Entity::setAttack(int atk) { 
+    attack = atk; 
+}
+
+void Entity::setDamage(int dmg) { 
+    damage = dmg; 
+}
+
+void Entity::setDefense(int def) { 
+    defence = def; 
+}
+
+void Entity::setXP(int exp) { 
+    xp = exp; 
+}
+
+void Entity::setGold(int g) { 
+    gold = g; 
+}
+
+void Entity::setLvl(int lvl) { 
+    level = lvl; 
+}
+
+void Entity::setCritDMG(int critdmg) { 
+    CRITDMG = critdmg; 
+}
+
+void Entity::setCritRate(int critrate) {
+    CRITRate = critrate; 
+}
 
 
 
 Entity::~Entity()
 {
-
 }
--- Entity.h
+++ Entity.h
@@ -9,16 +9,7 @@ protected:
 public:
 	Entity();
 
-    void setHealth(int hp);
-    void setAttack(int atk);
-    void setDamage(int dmg);
-    void setDefense(int defence);
-    void setXP(int xp);
-    void setGold(int gold);
-    void setLvl(int lvl);
-    void setCritDMG(int critDMG);
-    void setCritRate(int critRate);
-
+    Entity(int hp, int atk, int dmg, int def, int exp, int g, int lvl, int critdmg, int critrate);
 
     int getHealth() const;
     int getAttack() const;
@@ -30,9 +21,24 @@ public:
     int getCRITDMG() const;
     int getCRITRate() const;
 
+    void setHealth(int hp);
+    void setAttack(int atk);
+    void setDamage(int dmg);
+    void setDefense(int defence);
+    void setXP(int xp);
+    void setGold(int gold);
+    void setLvl(int lvl);
+    void setCritDMG(int critDMG);
+    void setCritRate(int critRate);
 
 
+    virtual void enemyPoison(int damage) {}
 
+    virtual void enemyBurn(int damage) {}
+
+	virtual bool isAlive() const { 
+        return health > 0; 
+    }
 
 	virtual ~Entity();
 

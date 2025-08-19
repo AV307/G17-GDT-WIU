0a430c69d6737c7afaedf9001d1f11b790bc2a0b
Zhi En <av30708zhien@gmail.com>
2025-08-19 14:57:33 +0800

    huge enemy update

--- Enemy.h
+++ Enemy.h
@@ -3,48 +3,17 @@
 #include<string>
 using namespace std;
 
-//enum class Enemytype{
-//    Basic,
-//    Elite,
-//    Boss
-//};
-class Enemy {
+class Enemy : public Entity {
 private:
-    string name;
-    int attack;
-    int health;
-    int damage;
-    int defence;
-    int xp;
-    int gold;
-    int level;
+
 
 public:
-    Enemy(string n, int h, int atk, int dmg, int def, int xp, int gold, int lvl);
-
-    void displayStatus() const;
-
-    string getName() const;
-    int getHealth() const;
-    int getAttack() const;
-    int getDamage() const;
-    int getDefence() const;
-    int getXP() const;
-    int getGold() const;
-    int getLvl() const;
-    enum getEnemytype(Enemytype);
-
-    void setHealth(int h);
-
-    void attackEnemy(Enemy* Enemy, int damage);
-
-    void levelUp(Enemy* enemy);
-
-    void initialiseEnemies(Enemy* enemies);
-
+    Enemy(string type, char status);
     ~Enemy();
+    void killEnemy();
+    char getStatus() const;
+    string getType() const;
 };
-
    
 
 

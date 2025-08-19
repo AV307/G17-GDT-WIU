7035cdaff8c8871e41b81aac3e70546d173bee3d
Zhi En <av30708zhien@gmail.com>
2025-08-19 10:07:31 +0800

    Updated stage functions

--- Stage.h
+++ Stage.h
@@ -1,17 +1,20 @@
 #pragma once
 #include "Room.h"
 #include <random>
+
+class Game;
 class Stage
 {
-public:
-    Stage();
-    ~Stage();
 private:
     char stageArray[100][100];
     Room* rooms[3];
+    Game* game;
 
     std::random_device rd;
     std::mt19937 gen;
     std::uniform_int_distribution<> dis;
+public:
+    Stage(Game* game);
+    ~Stage();
 };
 

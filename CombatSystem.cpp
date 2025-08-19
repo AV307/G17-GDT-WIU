9834af1f5f227112aeb22dece681c988147fb1dd
AEMizuki <ethanpang2298@gmail.com>
2025-08-19 11:29:51 +0800

    combat

--- CombatSystem.cpp
+++ CombatSystem.cpp
@@ -22,23 +22,81 @@ CombatSystem::~CombatSystem() {
 // +----------------------------------------------------------------------------------+ //
 
 void CombatSystem::printCombatScreen() {
-	string csBorderT1 = "// +----------------------------------------------+ //";
-	string csBorderT2 = "// +---------------------+  +---------------------+ //";
 
-	cout << csBorderT1 << endl;
-	cout << csBorderT2 << endl;
-	cout << "// +      Fight 'F'      +  +      Item 'I'       + //" << endl;
-	cout << csBorderT2 << endl;
-	cout << csBorderT1 << endl;
-	cout << "// +                   Run 'R'                    + //" << endl;
-	cout << csBorderT1 << endl;
-	cout << csBorderT1 << endl;
+	for (int n = 1; n < 11; n++) {
+		if (n == 1) { // Enemy Design Line
+			for (int i = 0; i < 5; i++) {
+				cout << "//";
+				for (int j = 0; j < 75; j++) {
+					cout << ' ';
+				}
+				cout << "//" << endl;
+			}
+		}
+		if (n == 2 || n == 4 || n == 8 || n == 10) { // Full Lines
+			cout << "// +";
+			for (int i = 0; i < 71; i++) {
+				cout << "-";
+			}
+			cout << "+ //" << endl;
+		}
+		if (n == 3) { // Text Dialogue Line
+			for (int i = 0; i < 3; i++) {
+				cout << "//";
+				for (int j = 0; j < 75; j++) {
+					cout << ' ';
+				}
+				cout << "//" << endl;
+			}
+		}
+		if (n == 5 || n == 7) { // 1/3 Division Lines
+			cout << "// +";
+			for (int i = 0; i < 21; i++) {
+				cout << "-";
+			}
+			for (int i = 0; i < 2; i++) {
+				cout << "+  +";
+				for (int j = 0; j < 21; j++) {
+					cout << "-";
+				}
+			}
+			cout << "+ //" << endl;
+		}
+		if (n == 6) { // Combat Option Line
+			cout << "// +      Fight 'F'      +  +       Item 'I'      +  +       Run 'R'       + //" << endl;
+		}
+		if (n == 9) { // 1/2 Division Line
+			cout << "// +";
+			for (int i = 0; i < 35; i++) {
+				cout << " ";
+			}
+			cout << "+";
+			for (int i = 0; i < 35; i++) {
+				cout << " ";
+			}
+			cout << "+ //" << endl;
+		}
+	}
+
+
+	
 
 	// Hard-Code Combat Screen Design
+	// cout << "//                                                                           //" << endl;  \
+	// cout << "//                                                                           //" << endl;   |
+	// cout << "//                                                                           //" << endl;    }  For Enemy Design
+	// cout << "//                                                                           //" << endl;   |
+	// cout << "//                                                                           //" << endl;  /
+	// cout << "// +-----------------------------------------------------------------------+ //" << endl;
+	// cout << "//                                                                           //" << endl;  \
+	// cout << "//                                                                           //" << endl;   }  For Text Dialogue
+	// cout << "//                                                                           //" << endl;  /
 	// cout << "// +-----------------------------------------------------------------------+ //" << endl;
 	// cout << "// +---------------------+  +---------------------+  +---------------------+ //" << endl;
 	// cout << "// +      Fight 'F'      +  +       Item 'I'      +  +       Run 'R'       + //" << endl;
 	// cout << "// +---------------------+  +---------------------+  +---------------------+ //" << endl;
+	// cout << "// +-----------------------------------------------------------------------+ //" << endl;
+	// cout << "// +                                   +                                   + //" << endl;  }  For Player & Enemy HP
 	// cout << "// +-----------------------------------------------------------------------+ //" << endl;
 }
 

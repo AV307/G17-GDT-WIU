dba0adba2456d349fc16d0be12adc3ac641e7923
AEMizuki <ethanpang2298@gmail.com>
2025-08-19 10:27:02 +0800

    combat

--- CombatSystem.cpp
+++ CombatSystem.cpp
@@ -2,11 +2,12 @@
 #include <iostream>
 #include <string>
 #include <conio.h>
+#include "Entity.h"
 
 using namespace std;
 
 CombatSystem::CombatSystem() {
-
+	turnsInBattle = 0;
 }
 
 CombatSystem::~CombatSystem() {
@@ -34,16 +35,54 @@ void CombatSystem::printCombatScreen() {
 	cout << csBorderT1 << endl;
 
 	// Hard-Code Combat Screen Design
-	// cout << "// +----------------------------------------------+ //" << endl;
-	// cout << "// +---------------------+  +---------------------+ //" << endl;
-	// cout << "// +      Fight 'F'      +  +      Item 'I'       + //" << endl;
-	// cout << "// +---------------------+  +---------------------+ //" << endl;
-	// cout << "// +----------------------------------------------+ //" << endl;
-	// cout << "// +                   Run 'R'                    + //" << endl;
-	// cout << "// +----------------------------------------------+ //" << endl;
-	// cout << "// +----------------------------------------------+ //" << endl;
+	// cout << "// +-----------------------------------------------------------------------+ //" << endl;
+	// cout << "// +---------------------+  +---------------------+  +---------------------+ //" << endl;
+	// cout << "// +      Fight 'F'      +  +       Item 'I'      +  +       Run 'R'       + //" << endl;
+	// cout << "// +---------------------+  +---------------------+  +---------------------+ //" << endl;
+	// cout << "// +-----------------------------------------------------------------------+ //" << endl;
 }
 
-void CombatSystem::fightPVE() {
+
+
+
+
+// +----------------------------------------------------------------------------------+ //
+// Function Name: fightPVE
+// Description: To carry out the "Fight" action between Player and Enemy
+// Parameter(s): None
+// Return(s): None
+// +----------------------------------------------------------------------------------+ //
+
+void CombatSystem::fightPVE(Entity& player, Entity& specifiedEnemy) {
+
+}
+
+
+
+
+
+// +----------------------------------------------------------------------------------+ //
+// Function Name: itemPVE
+// Description: To carry out the "Item" action between Player and Enemy
+// Parameter(s): None
+// Return(s): None
+// +----------------------------------------------------------------------------------+ //
+
+void CombatSystem::itemPVE(Entity& player, Entity& specifiedEnemy) {
+
+}
+
+
+
+
+
+// +----------------------------------------------------------------------------------+ //
+// Function Name: runPVE
+// Description: To carry out the "Run" action between Player and Enemy
+// Parameter(s): None
+// Return(s): None
+// +----------------------------------------------------------------------------------+ //
+
+void CombatSystem::runPVE(Entity& player, Entity& specifiedEnemy) {
 
 }

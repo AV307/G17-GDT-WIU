c9cac69f8d436fa88ab0fc0d9cebf3adffcd4e80
DinosaurGoesExtinct <unavailablenottaken@gmail.com>
2025-08-21 10:23:22 +0800

    a

--- G17-GDT-WIU.cpp
+++ G17-GDT-WIU.cpp
@@ -1,23 +1,10 @@
 ﻿#include "Game.h"
 #include "CombatSystem.h"
 
-#include <Windows.h>
 #include <iostream>
 
 int main()
 {
-    // Get the input handle
-    HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
-    DWORD prev_mode;
-    GetConsoleMode(hInput, &prev_mode);
-
-    // Turn off Quick Edit and insert mode
-    DWORD new_mode = prev_mode;
-    new_mode &= ~ENABLE_QUICK_EDIT_MODE;
-    new_mode &= ~ENABLE_INSERT_MODE;
-
-    SetConsoleMode(hInput, new_mode);
-
     Game game;
     CombatSystem CombatSystem;
 

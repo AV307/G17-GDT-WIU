42503bcbdd19ad26c638e831d999567e4f13b066
welly <cskj33z@gmail.com>
2025-08-25 13:12:37 +0800

    ki

--- G17-GDT-WIU.cpp
+++ G17-GDT-WIU.cpp
@@ -9,8 +9,6 @@ int main()
     srand(static_cast<unsigned int>(time(0)));
     Game game;
     CombatSystem CombatSystem;
-    Player player;
-    
     while (true) {
         game.doTurn(CombatSystem);
     }

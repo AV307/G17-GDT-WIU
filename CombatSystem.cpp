3bac6f4a5437da53e1216f2271e46fb718c3adf5
AEMizuki <ethanpang2298@gmail.com>
2025-08-25 17:50:16 +0800

    Combat

--- CombatSystem.cpp
+++ CombatSystem.cpp
@@ -620,25 +620,40 @@ void CombatSystem::runPVE(Entity& player, Entity& specifiedEnemy) {
 	case 'y':                                                                                                            // If player carries on anyway
 		if (consDeterminant == 0) {                                                                                      // 1st consequence
 			player.setHealth(player.getHealth() * 3 / 4);                                                                // Lose 25% of current HP
-			static_cast<Player&>(player).setIsInCombat(false);                                                           // Player exits combat after changes are made
-			static_cast<Player&>(player).setJustLeftCombat(true);                                                        // Player gains invulnerability from entering the fight again
-			static_cast<Player&>(player).setCombatIsWon(false);                                                          // Reset player's win state (precautionary)
+			char carryOn = _getch();
+			setTextDialogue("You ran... but lost something along the way (-25% HP)");
+
+			if (carryOn == 'y') {
+				static_cast<Player&>(player).setIsInCombat(false);                                                           // Player exits combat after changes are made
+				static_cast<Player&>(player).setJustLeftCombat(true);                                                        // Player gains invulnerability from entering the fight again
+				static_cast<Player&>(player).setCombatIsWon(false);                                                          // Reset player's win state (precautionary)
+			}
 		}
 		else if (consDeterminant == 1) {                                                                                 // 2nd consequence
 			player.setXP(player.getXP() * 3 / 4);                                                                        // Lose 25% of current XP
-			static_cast<Player&>(player).setIsInCombat(false);                                                           // Player exits combat after changes are made
-			static_cast<Player&>(player).setJustLeftCombat(true);                                                        // Player gains invulnerability from entering the fight again
-			static_cast<Player&>(player).setCombatIsWon(false);                                                          // Reset player's win state (precautionary)
+			char carryOn = _getch();
+			setTextDialogue("You ran... but lost something along the way (-25% XP)");
+
+			if (carryOn == 'y') {
+				static_cast<Player&>(player).setIsInCombat(false);                                                           // Player exits combat after changes are made
+				static_cast<Player&>(player).setJustLeftCombat(true);                                                        // Player gains invulnerability from entering the fight again
+				static_cast<Player&>(player).setCombatIsWon(false);                                                          // Reset player's win state (precautionary)
+			}
 		}
 		else if (consDeterminant == 2) {                                                                                 // 3rd consequence
 			player.setGold(player.getGold() - 50);                                                                       // Lose 50 Gold
-			static_cast<Player&>(player).setIsInCombat(false);                                                           // Player exits combat after changes are made
-			static_cast<Player&>(player).setJustLeftCombat(true);                                                        // Player gains invulnerability from entering the fight again
-			static_cast<Player&>(player).setCombatIsWon(false);                                                          // Reset player's win state (precautionary)
+			char carryOn = _getch();
+			setTextDialogue("You ran... but lost something along the way (-50 Gold)");
+
+			if (carryOn == 'y') {
+				static_cast<Player&>(player).setIsInCombat(false);                                                           // Player exits combat after changes are made
+				static_cast<Player&>(player).setJustLeftCombat(true);                                                        // Player gains invulnerability from entering the fight again
+				static_cast<Player&>(player).setCombatIsWon(false);                                                          // Reset player's win state (precautionary)
+			}
 		}
 		else {                                                                                                           // Off chance
 			setTextDialogue("Uh oh, you failed to run, the enemy stopped you before you could. Press Y to continue");    // Player fails to run, getting "dragged" back to fight
-			int carryOn = _getch();                                                                                      // To receive player input as a move to next dialogue function
+			char carryOn = _getch();                                                                                     // To receive player input as a move to next dialogue function
 			switch (carryOn) {
 			case 'y':
 				int retaliateDeterminant = rand() % 2;
@@ -653,10 +668,12 @@ void CombatSystem::runPVE(Entity& player, Entity& specifiedEnemy) {
 					player.setHealth(player.getHealth() * 8 / 10);                                                       // Player loses 20% of current HP from colliding
 					static_cast<Player&>(player).setIsInCombat(true);                                                    // Player stays in combat
 				}
+				break;
 			}
 		}
 	case 'n':                                                                                                            // If player backs out from running
 		setTextDialogue("You snapped out of it, kept your head in the game");                                            // Let's them stay in the game
+		break;
 	}
 }
 
